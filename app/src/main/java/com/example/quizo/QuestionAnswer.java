5d8be7c4c0de33e9c98023a7adce8255ee58a135
ankit142jaiswal <ankit2020cs142@abesit.edu.in>
2023-05-28 18:10:57 +0530

    Final Commit

---
+++ app/src/main/java/com/example/quizo/QuestionAnswer.java
@@ -0,0 +1,45 @@
+package com.example.quizo;
+
+public class QuestionAnswer {
+    public static String[] question ={
+            "The coordination number in hcp is",
+            "The space lattice of graphite is ",
+            "The existence of substance in more than one solid modification is known as",
+            "Which of the following is not a crystalline solid",
+            "Which of the following is an amorphous solid",
+            "An example of a covalent crystalline solid is ",
+            "Among solids,the highest melting point is exhibited by",
+            "Which of the following exists as covalent crystals in solid state ?",
+            "Which one of the following forms a molecular solid when solidified ?",
+            "Graphite cannot be classified as"
+    };
+    public static String[][] choices ={
+            {"6","12","18","24"},
+            {"Cubic","Tetragonal","Rhombic","Hexagonal"},
+            {"Isomorphism","Polymorphism","Amorphism","Allotropy"},
+            {"KCL","CsCL","Glas","Rhombic","S"},
+            {"Graphite (C)","Quartz glass (SiO2)","Chrome alum","Silicon carbide (SiC)"},
+            {"Si","Al","NaF","Ar"},
+            {"Covalent solids","Ionic solids","Pseudo solids","Molecular solids"},
+            {"Iodine","Silicon","Sulphur","Phosphorus"},
+            {"Silicon carbide","Calcium fluoride","Rock salt","Methane"},
+            {"Conducting solid","Network solid","Covalent solid","Ionic solid"}
+
+
+    };
+    public static String[] correctAnswer ={
+            "12",
+            "Hexagonal",
+            "Polymorphism",
+            "Glas",
+            "Quartz glass (SiO2)",
+            "Si",
+            "Covalent solids",
+            "Silicon",
+            "Methane",
+            "Ionic solid"
+
+
+
+    };
+}
